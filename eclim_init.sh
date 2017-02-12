e1f741c518eafec0f153577667fdc501a7064064
w0lker <w0lker.tg@gmail.com>
2017-02-12 17:18:37 +0800

    change plugin‘

--- eclim_init.sh
+++ eclim_init.sh
@@ -6,17 +6,13 @@ set -e
 sleep 1
 
 init_flag="/sbin/_init_sucess_"
-if [ ! -f $init_flag ];then
-    su -l eclim -c "cd /home/eclim && ./eclipse/eclipse -nosplash -consolelog -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/releases/neon -installIU org.eclipse.dltk.core.feature.group"
+if [ ! -f $init_flag ]; then
     su -l eclim -c "cd /home/eclim && ./eclipse/eclipse -nosplash -consolelog -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/releases/neon -installIU org.eclipse.wst.web_ui.feature.feature.group"
+    su -l eclim -c "cd /home/eclim && ./eclipse/eclipse -nosplash -consolelog -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/releases/neon -installIU org.eclipse.dltk.core.feature.group"
     su -l eclim -c "cd /home/eclim && ./eclipse/eclipse -nosplash -consolelog -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/releases/neon -installIU org.eclipse.jdt.feature.group"
-    su -l eclim -c "cd /home/eclim && ./eclipse/eclipse -nosplash -consolelog -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/releases/neon -installIU org.codehaus.groovy.eclipse.feature.feature.group"
-    su -l eclim -c "cd /home/eclim && ./eclipse/eclipse -nosplash -consolelog -debug -application org.eclipse.equinox.p2.director -repository http://download.eclipse.org/releases/neon -installIU org.scala-ide.sdt.feature.feature.group"
 
     su -l eclim -c "cd /home/eclim && java -Dvim.skip=true -Declipse.home=/home/eclim/eclipse -jar eclim_2.6.0.jar install && rm -rf eclim_2.6.0.jar"
     touch $init_flag
-else
-    echo "All ready!"
 fi
 
 /etc/init.d/xvfb stop
